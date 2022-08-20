56a0ddab3966eca9590d0a3b9186595445764e27
suttipong markchoochit <baysuttipong@gmail.com>
2022-08-20 21:22:51 +0700

    NewGame

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ import WordCard from './WordCard';
 const word = ['Thailand', 'Laos', 'Philippines', 'Singapore', 'Brunei', 'Malaysia', 'Myanmar', 'Indonesia', 'Vietnam', 'cambodia'];
 const random = word[Math.floor(Math.random()*word.length)];
 
+function refreshPage() {
+  window.location.reload(false);
+}
+
 function App() {
   return (
     <div>
       <WordCard value = {random}/>
+      <button
+        type="button"
+        className='button'
+        onClick={refreshPage}
+        >New Game</button>
     </div>
   );
 }
