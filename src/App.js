7a38e85f5e18755ba57de58e189f04d72dc727a1
suttipong markchoochit <baysuttipong@gmail.com>
2022-08-20 20:40:30 +0700

    AddingWord

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Hello";
+const word1 = "Hello";
+const word2 = "Computer";
+const word3 = "Engineer";
+const word4 = "Bye";
 function App() {
   return (
     <div>
-      <WordCard value ={word}/>
+      <WordCard value ={word1}/>
+      <WordCard value ={word2}/>
+      <WordCard value ={word3}/>
+      <WordCard value ={word4}/>
     </div>
   );
 }
