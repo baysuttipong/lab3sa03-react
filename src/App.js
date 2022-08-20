c2c37d2fee8ab8213fd08bbd9aaeee5f286de823
suttipong markchoochit <baysuttipong@gmail.com>
2022-08-20 21:09:40 +0700

    randomword

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word1 = "Hello";
-const word2 = "Computer";
-const word3 = "Engineer";
-const word4 = "Bye";
+const word = ['Thailand', 'Laos', 'Philippines', 'Singapore', 'Brunei', 'Malaysia', 'Myanmar', 'Indonesia', 'Vietnam', 'cambodia'];
+const random = word[Math.floor(Math.random()*word.length)];
+
 function App() {
   return (
     <div>
-      <WordCard value ={word1}/>
-      <WordCard value ={word2}/>
-      <WordCard value ={word3}/>
-      <WordCard value ={word4}/>
+      <WordCard value = {random}/>
     </div>
   );
 }
